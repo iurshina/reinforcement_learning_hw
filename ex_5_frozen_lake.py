9095e0843fc7924c8fed147442760dfdf405a898
Anastasiia Iurshina <git-commit@iurshina.de>
2018-06-05 10:18:03 +0200

    Ex 5 - frozen lake q-learning + deterministic env

--- ex_5_frozen_lake.py
+++ ex_5_frozen_lake.py
@@ -1,17 +1,15 @@
-import gym
 import numpy as np
-from collections import defaultdict
+from gym.envs.toy_text.frozen_lake import FrozenLakeEnv
 
-env = gym.make("FrozenLake-v0")
 
 alpha = 0.4
 gamma = 0.1
 epsilon = 0.33
 
 
-def action(Q, obs):
+def action(Q, obs, space_n):
     if np.random.rand() <= epsilon:
-        return np.random.randint(0, env.action_space.n)
+        return np.random.randint(0, space_n)
     else:
         return np.argmax(Q[obs])
 
@@ -22,25 +20,25 @@ def action(Q, obs):
 # UP = 3
 def get_policy(Q):
     pi = {}
-    for k in Q:
-        pi[k] = np.argmax(Q[k])
+    for i in range(0, len(Q)):
+        pi[i] = np.argmax(Q[i])
     return pi
 
 
 def get_v(Q):
     V = {}
-    for k in Q:
-        V[k] = np.max(Q[k])
+    for i in range(0, len(Q)):
+        V[i] = np.max(Q[i])
     return V
 
 
 # task 2
-def sarsa(steps):
-    Q = defaultdict(lambda: [0.9, 0.9, 0.9, 0.9])
+def sarsa(env, steps):
+    Q = np.zeros([env.observation_space.n, env.action_space.n])
     t = 0
     for i in range(steps):
         observation = env.reset()
-        act = action(Q, observation)
+        act = action(Q, observation, env.action_space.n)
 
         prev_obs = None
         prev_act = None
@@ -48,7 +46,7 @@ def sarsa(steps):
         done = False
         while not done:
             observation, reward, done, info = env.step(act)
-            act = action(Q, observation)
+            act = action(Q, observation, env.action_space.n)
 
             if prev_obs is not None:
                 prev_q = Q[prev_obs][prev_act]
@@ -67,8 +65,53 @@ def sarsa(steps):
     return Q, get_v(Q), get_policy(Q)
 
 
+# task 3
+def q_learning(env, steps):
+    Q = np.zeros([env.observation_space.n, env.action_space.n])
+    t = 0
+    for i in range(steps):
+        observation = env.reset()
+        act = action(Q, observation, env.action_space.n)
+
+        prev_obs = None
+        prev_act = None
+
+        done = False
+        while not done:
+            observation, reward, done, info = env.step(act)
+            act = action(Q, observation, env.action_space.n)
+
+            if prev_obs is not None:
+                prev_q = Q[prev_obs][prev_act]
+                if done:
+                    Q[prev_obs][prev_act] = prev_q + alpha * (reward - prev_q)
+                else:
+                    Q[prev_obs][prev_act] = prev_q + alpha * (reward + gamma * np.max(Q[observation]) - prev_q)
+
+            prev_obs = observation
+            prev_act = act
+
+            t += 1
+
+    print "Avg length of an episode:", t / steps
+
+    return Q, get_v(Q), get_policy(Q)
+
+
+# todo: draw everything
 def main():
-    Q, V, pi = sarsa(100000)
+    env = FrozenLakeEnv(is_slippery=True)
+
+    # doesn't really work
+    Q, V, pi = sarsa(env, 1000)
+    Q, V, pi = q_learning(env, 1000)
+
+    # deterministic works
+    env = FrozenLakeEnv(is_slippery=False)
+
+    Q, V, pi = sarsa(env, 1000)
+    Q, V, pi = q_learning(env, 1000)
+
     print
 
 
